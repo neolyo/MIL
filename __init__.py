ac0af3741b88cee78369e6f01181e1ca404fa5bb
Xiaoxiao Ma <exiaoxiao.ma@gmail.com>
2020-09-13 10:59:17 +1000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2020/08/29 12:15:03
+@Author  :   Xiaoxiao Ma, Jia Wu
+@Version :   1.0
+@Contact :   xiaoxiao.ma2@hdr.mq.edu.au
+@Desc    :   Contact author for details
+'''
+
